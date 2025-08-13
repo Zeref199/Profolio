b73aaaa26d5c1d81b9d9ac32eaeabe4dac4d6c5a
Otmane ARSALANE <otmane.arsalane@cegedim-activ.com>
2025-08-13 14:24:16 +0100

    updated 2025

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,29 +1,30 @@
 import { Component, Renderer2 } from '@angular/core';
-import { Title } from '@angular/platform-browser'
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.css']
+  styleUrls: ['./resume.component.css'],
 })
 export class ResumeComponent {
-
   isWorkExperienceOpen: boolean = false;
   isEducationOpen: boolean = false;
   isCertificationOpen: boolean = false;
   isSkillsOpen: boolean = false;
 
-  constructor(private titleService: Title, private renderer: Renderer2){
+  constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle('Arsalane Otmane - Resume');
   }
 
-  DownloadFile(){
+  DownloadFile() {
     const link = this.renderer.createElement('a');
     link.setAttribute('target', '_blank');
-    link.setAttribute('href', 'https://shopoume.s3.eu-north-1.amazonaws.com/Profolio/Resume.pdf"');
+    link.setAttribute(
+      'href',
+      'https://shopoume.s3.eu-north-1.amazonaws.com/Profolio/CV_ARSALANE_OTMANE_2025.pdf'
+    );
     link.setAttribute('download', 'Resume.pdf');
     link.click();
     link.remove();
   }
-
 }
